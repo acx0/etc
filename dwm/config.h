b2658b7e2e0bd4100eb44ea8f1f1a62d1ce127dc
Sam Lidder <sam.lidder@gmail.com>
2011-07-26 01:41:36 -0400

    added flag to display dmenu on bottom of screen

--- dwm/config.h
+++ dwm/config.h
@@ -45,7 +45,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
 #include "shiftview.c"
