25223e986e559072afe8d7c34991b9ddb6fdc350
Sam Lidder <sam.lidder@gmail.com>
2011-07-30 02:06:47 -0400

    fixed togglefloating key binding + made layout selection for float more dynamic

--- dwm/config.h
+++ dwm/config.h
@@ -75,12 +75,12 @@ static Key keys[] = {
     { MODKEY,                       XK_Tab,    view,           {0} },
     { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
     /*{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },*/
-    /*{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },*/
     /*{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },*/
     /*{ MODKEY,                       XK_space,  setlayout,      {0} },*/
+    { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[sizeof(layouts) / sizeof(layouts[0]) - 2]} },
     { MODKEY,                       XK_space,  nextlayout,     {0} },
     { MODKEY|ShiftMask,             XK_space,  prevlayout,     {0} },
-    { MODKEY,                       XK_f,      togglefloating, {0} },
+    { MODKEY|ControlMask,           XK_space,  togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
     { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
     { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
